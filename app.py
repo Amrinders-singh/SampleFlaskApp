a6b0a86ed00732985eb008b0a17bb98b59b4b56d
Amrinder Singh <43899719+Amrinders-singh@users.noreply.github.com>
2025-05-25 13:25:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def create_app():
         x = 11233
         print(x)
         print("inside home function")
-        return 'Hurray hell of heaven, + Lets come together and dive' 
+        return 'Hurray hell of heaven, Lets come' 
 
     @app.route('/test')
     def test():
